63a0cfb575e79fb545ec899e37e933c8232f112d
Shubh9907 <Shubhamv60047@gmail.com>
2021-12-02 23:36:51 +0530

    Added: Code for checking all the details of the contact are correct

--- AddressBook.js
+++ AddressBook.js
@@ -10,73 +10,141 @@ class Contacts {
     phoneNumber;
     email;
 
-    constructor(firstName,lastName,address,city,state,zip,phoneNumber,email) {
-        this.firstName=firstName;
-        this.lastName=lastName;
-        this.address=address;
-        this.city=city;
-        this.state=state;
-        this.zip=zip;
-        this.phoneNumber=phoneNumber;
-        this.email=email;
+    constructor(firstName, lastName, address, city, state, zip, phoneNumber, email) {
+
+        let NAME_PATTERN = RegExp("^[A-Z][a-z]{2,}$");
+        let ZIP_PATTERN = RegExp("^[0-9]{6}$");
+        let NUM_PATTERN = RegExp("^[1-9][0-9]{9}$");
+        let EMAIL_PATTERN = RegExp("^[a-zA-Z0-9]+([_+-.][a-zA-Z0-9]+)?[@][a-z]{2,}[.][a-z]{2,4}([.][a-z]{2,3})?$");
+        this.flag = 0;
+
+        if (NAME_PATTERN.test(firstName)) {
+            this.firstName = firstName;
+        } else {
+            console.log("First name is incorrect");
+        }
+
+        if (NAME_PATTERN.test(lastName)) {
+            this.lastName = lastName;
+        } else {
+            console.log("Last name is incorrect");
+        }
+
+        this.address = address;
+        this.city = city;
+        this.state = state;
+
+        if (ZIP_PATTERN.test(zip)) {
+            this.zip = zip;
+        } else {
+            console.log("Incorrect Zip");
+        }
+
+        if (NUM_PATTERN.test(phoneNumber)) {
+            this.phoneNumber = phoneNumber;
+        } else {
+            console.log("incorrect phone Number");
+        }
+
+        if (EMAIL_PATTERN.test(email)) {
+            this.email = email;
+        } else {
+            console.log("Incorrect email");
+        }
     }
 
     get firstName() {
         return this.firstName;
     }
-    set firstName(firstName){
-        this.firstName=firstName;
+
+    set firstName(firstName) {
+        let NAME_PATTERN = RegExp("^[A-Z][a-z]{3,}$");
+        if (NAME_PATTERN.test(firstName)) {
+            this.firstName = firstName;
+        } else {
+            console.log("First name is incorrect");
+        }
     }
+
     get lastName() {
         return this.lastName;
     }
-    set lastName(lastName){
-        this.lastName=lastName;
+    set lastName(lastName) {
+        if (NAME_PATTERN.test(lastName)) {
+            this.lastName = lastName;
+        } else {
+            console.log("Last name is incorrect");
+        }
     }
+
     get address() {
         return this.address;
     }
-    set address(address){
-        this.address=address;
+
+    set address(address) {
+        this.address = address;
     }
+
     get city() {
         return this.city;
     }
-    set city(city){
-        this.city=city;
+
+    set city(city) {
+        this.city = city;
     }
+
     get state() {
         return this.state;
     }
-    set state(state){
-        this.state=state;
+
+    set state(state) {
+        this.state = state;
     }
+
     get zip() {
         return this.zip;
     }
-    set zip(zip){
-        this.zip=zip;
+
+    set zip(zip) {
+        if (ZIP_PATTERN.test(zip)) {
+            this.zip = zip;
+        } else {
+            console.log("Incorrect Zip");
+        }
     }
+
     get phoneNumber() {
         return this.phoneNumber;
     }
-    set phoneNumber(phoneNumber){
-        this.phoneNumber=phoneNumber;
+
+    set phoneNumber(phoneNumber) {
+        if (NUM_PATTERN.test(phoneNumber)) {
+            this.phoneNumber = phoneNumber;
+        } else {
+            console.log("incorrect phone Number");
+        }
     }
+
     get email() {
         return this.email;
     }
-    set email(email){
-        this.email=email;
+    set email(email) {
+        if (EMAIL_PATTERN.test(email)) {
+            this.email = email;
+        } else {
+            console.log("Incorrect email");
+        }
     }
 }
 
 let contactList = new Array;
 
-function addContact(firstName,lastName,address,city,state,zip,phoneNumber,email) {
-    let contact = new Contacts(firstName,lastName,address,city,state,zip,phoneNumber,email);
-    contactList.push(contact);
+function addContact(firstName, lastName, address, city, state, zip, phoneNumber, email) {
+    let contact = new Contacts(firstName, lastName, address, city, state, zip, phoneNumber, email);
+    if (Contacts.flag == 0) {
+        contactList.push(contact);
+    }
 }
 
-addContact("shubham","verma","sdssdf","sdsd","sdsd","sdsd","sfsd","sdsdf");
+addContact("Shubham", "Verma", "sdssdf", "sdsd", "sdsd", "123456", "1234567890", "shubham@bridgelabz.com");
 console.log(contactList);
